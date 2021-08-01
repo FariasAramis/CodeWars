1f5834d47ce04b9cfa746d6b6436a29c71e27894
FariasAramis <fariasaramis@gmail.com>
2021-08-01 20:15:58 -0300

    Uses REGEXP_MATCHES,REGEXP_REPLACE

---
+++ SQL/7_kyu/Trimming a Field.sql
@@ -0,0 +1,34 @@
+/*
+You have access to a table of monsters as follows:
+
+monsters schema
+
+id
+name
+legs
+arms
+characteristics
+The monsters in the provided table have too many characteristics, they really only need one each. Your job is to trim the characteristics down so that each monster only has one. If there is only one already, provide that. If there are multiple, provide only the first one (don't leave any commas in there).
+
+You must return a table with the format as follows:
+
+output schema
+
+id
+name
+characteristic
+Order by id
+*/
+
+SELECT id,
+       name,
+       (regexp_matches(characteristics,'([A-Za-z]+),?'))[1] as characteristic
+FROM monsters
+ORDER BY id
+
+--ALTERNATIVE
+SELECT id,
+       name,
+       regexp_replace(characteristics, ',.*', '') AS characteristic
+FROM monsters
+ORDER BY id
