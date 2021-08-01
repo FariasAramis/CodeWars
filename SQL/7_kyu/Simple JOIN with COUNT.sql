f4b4ccd4371a3f3be187ca5599b990c9ec34ff4a
FariasAramis <fariasaramis@gmail.com>
2021-07-31 22:47:15 -0300

    Uses COUNT,JOIN, GROUP BY

---
+++ SQL/7_kyu/Simple JOIN with COUNT.sql
@@ -0,0 +1,17 @@
+/*
+For this challenge you need to create a simple SELECT statement that will return all columns from the people table, and join to the toys table so that you can return the COUNT of the toys
+
+people table schema
+id
+name
+toys table schema
+id
+name
+people_id
+You should return all people fields as well as the toy count as "toy_count".
+https://www.codewars.com/kata/580918e24a85b05ad000010c
+*/
+SELECT p.*, COUNT(t.id) AS toy_count
+FROM people p
+  JOIN toys t ON p.id=t.people_id
+GROUP BY p.id, p.name
